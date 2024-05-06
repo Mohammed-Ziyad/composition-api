23ed01b66aa0cb4061eb9d8d02cb83ab7467120e
Mohammed-Ziyad <mohammedaman8990@gmail.com>
2024-05-06 17:41:09 +0300

    use watch with a variable , array and object

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,41 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Use Mixins with props" />
+    <h1>Use watch</h1>
+    <h2>Counter is : {{ counter }}</h2>
+    <button @click="counter++">Counter</button>
+    <h2>New value : {{ myObject.newval }} Old value : {{ myObject.oldval }}</h2>
+    <button @click="myArray.push(counter)">Push Counter Value</button>
+    <h2>The length of the array : {{ myArray.length }}</h2>
   </div>
 </template>
 
-<script>
+<script setup>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+import { ref, watch } from "vue";
+const counter = ref(0);
+const myArray = ref([]);
+const myObject = ref({ newval: 0, oldval: 0 });
+// watch
+watch(
+  () => counter.value,
+  (newval, oldval) => {
+    console.log("newval: ", newval, "oldval: ", oldval);
+    myObject.value.newval = newval;
+    myObject.value.oldval = oldval;
+  }
+);
+watch(
+  () => myArray.value,
+  (newvalue) => {
+    console.log("newvalue to array: ", newvalue);
   },
-};
+  { deep: true }
+);
 </script>
+<style scoped lang="scss">
+button {
+  margin-right: 10px;
+}
+</style>
