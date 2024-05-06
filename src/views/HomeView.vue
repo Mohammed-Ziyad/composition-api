b9f5eb5861aa04197244366ded7820b96c316569
Mohammed-Ziyad <mohammedaman8990@gmail.com>
2024-05-06 16:51:43 +0300

    Use Mixins with props

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Use Mixins with props" />
   </div>
 </template>
 
